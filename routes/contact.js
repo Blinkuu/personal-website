6101ae31afd80d0dbe5a65546a86675eed6fdbbc
Łukasz Gut <blinekgames@gmail.com>
2019-09-21 20:06:39 +0200

    1.0.1a

--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
+const request = require("request");
 
 /* GET home page. */
 router.get("/", (req, res) => {
@@ -8,8 +9,38 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
+  const [name, email, subject, message, recaptcha] = [
+    req.body.name,
+    req.body.email,
+    req.body.subject,
+    req.body.message,
+    req.body["g-recaptcha-response"]
+  ];
 
+  request.post(
+    "https://www.google.com/recaptcha/api/siteverify",
+    {
+      form: {
+        secret: process.env.RECAPTCHA_SECRET,
+        response: recaptcha
+      }
+    },
+    (err, httpResponse, body) => {
+      const parsedBody = JSON.parse(body);
+
+      if (parsedBody.success === true) {
+        sendMail(res, name, email, subject, message);
+      } else {
+        res.render("error", {
+          message: "Failed to bypass reCAPTCHA!",
+          error: ""
+        });
+      }
+    }
+  );
+});
+
+const sendMail = (res, name, email, subject, message) => {
   const smtpTransport = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
@@ -23,18 +54,20 @@ router.post("/", (req, res) => {
   const mailOptions = {
     from: "Your sender info here", // This is ignored by Gmail
     to: process.env.GMAIL_USER,
-    subject: req.body.subject,
-    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+    subject: subject,
+    text: `${name} (${email}) says: ${message}`
   };
 
-  smtpTransport.sendMail(mailOptions, (error, response) => {
+  smtpTransport.sendMail(mailOptions, error => {
     if (error) {
-      res.render("error", { message: "Failed to send an email!" , error: error}); // Show a page indicating failure
-    }
-    else {
+      res.render("error", {
+        message: "Failed to send an email!",
+        error: error
+      });
+    } else {
       res.redirect("/");
     }
   });
-});
+};
 
 module.exports = router;
