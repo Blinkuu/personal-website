5845adc86e121577a18dcac7a8568f1e81db6058
Łukasz Gut <blinekgames@gmail.com>
2019-10-05 19:25:58 +0200

    Implemented post updating from admin panel

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,33 +3,37 @@ const router = express.Router();
 const Post = require("../models/post");
 
 router.get("/", (req, res) => {
-  Post.find({}).lean().exec((err, posts) => {
-    if (!err) {
-      posts.forEach((post) => {
-        post.date = new Date(post.date).toUTCString();
-      });
-      res.render("home", { posts: posts });
-    } else {
-      res.render("error", {
-        message: "Failed fetch posts from database!",
-        error: err
-      });
-    }
-  });
+  Post.find({})
+    .lean()
+    .exec((err, posts) => {
+      if (!err) {
+        posts.forEach(post => {
+          post.date = new Date(post.date).toUTCString();
+        });
+        res.render("home", { posts: posts });
+      } else {
+        res.render("error", {
+          message: "Failed fetch posts from database!",
+          error: err
+        });
+      }
+    });
 });
 
 router.get("/:postId", (req, res) => {
-  Post.findOne({_id : req.params.postId}).lean().exec((err, post) => {
-    if (!err) {
-      post.date = new Date(post.date).toUTCString();
-      res.render("post", { post: post });
-    } else {
-      res.render("error", {
-        message: "Failed fetch post from database!",
-        error: err
-      });
-    }
-  });
+  Post.findOne({ _id: req.params.postId })
+    .lean()
+    .exec((err, post) => {
+      if (!err) {
+        post.date = new Date(post.date).toUTCString();
+        res.render("post", { post: post });
+      } else {
+        res.render("error", {
+          message: "Failed fetch post from database!",
+          error: err
+        });
+      }
+    });
 });
 
 router.get("/home", (req, res) => {
@@ -38,7 +42,13 @@ router.get("/home", (req, res) => {
 
 router.post("/", async (req, res) => {
   if (req.isAuthenticated()) {
-    const newPost = new Post({ author: req.body.author, date: Date.now(), title: req.body.title, introduction: req.body.introduction, content: req.body.content });
+    const newPost = new Post({
+      author: req.body.author,
+      date: Date.now(),
+      title: req.body.title,
+      introduction: req.body.introduction,
+      content: req.body.content
+    });
     const result = await newPost.save();
     res.redirect("/admin");
   } else {
@@ -48,12 +58,39 @@ router.post("/", async (req, res) => {
 
 router.delete("/:postId", async (req, res) => {
   if (req.isAuthenticated()) {
-    console.log("PARAMID: " + req.params.postId);
     const result = await Post.deleteOne({ _id: req.params.postId }).exec();
     res.redirect(303, "/admin");
   } else {
     res.redirect(303, "/");
   }
-})
+});
+
+router.put("/:postId", async (req, res) => {
+  if (req.isAuthenticated()) {
+    const result = await Post.updateOne(
+      { _id: req.params.postId },
+      {
+        author: req.body.data.author,
+        date: Date.now(),
+        title: req.body.data.title,
+        introduction: req.body.data.introduction,
+        content: req.body.data.content
+      }
+    ).exec();
+    res.redirect(303, "/admin");
+  } else {
+    res.redirect(303, "/");
+  }
+});
+
+router.get("/edit/:postId", async (req, res) => {
+  console.log(req.params.postId);
+  if (req.isAuthenticated()) {
+    const result = await Post.findById(req.params.postId).exec();
+    res.send(result);
+  } else {
+    res.redirect("/login");
+  }
+});
 
 module.exports = router;
--- public/javascripts/axios-requests.js
+++ public/javascripts/axios-requests.js
@@ -1,4 +1,50 @@
-function deleteRequest(postId) {
-    axios.delete("/" + postId, { data: { foo: "bar" } });
-    window.location.replace("/admin");
+async function deleteRequest(route) {
+  const result = await axios.delete(route, { data: { foo: "bar" } });
+  window.location.replace("/admin");
+}
+
+async function getRequest(route) {
+  const result = await axios.get(route);
+
+  $("div.page-content .row:nth-last-child(2)").after(`
+  <div class="row pt-3">
+    <div class="col-12">
+      <div class="form-update" autocomplete="off">
+          <div class="form-label-group">
+            <input type="text" id="inputAuthor" name="author" class="form-control" placeholder="Author" value="${result.data.author}" required autofocus/>
+            <label for="inputAuthor">Author</label>
+          </div>
+          <div class="form-label-group">
+            <input type="text" id="inputTitle" name="title" class="form-control" placeholder="Title" value="${result.data.title}" required autofocus/>
+            <label for="inputTitle">Title</label>
+          </div>
+          <div class="form-label-group">
+              <textarea rows="2" id="inputIntroduction" name="introduction" class="form-control" placeholder="Introduction" required autofocus>${result.data.introduction}</textarea>
+              <label for="inputIntroduction">Introduction</label>
+          </div>
+          <div class="form-label-group">
+              <textarea rows="6" id="inputEdit" name="edit" class="form-control" placeholder="Content" required autofocus>${result.data.content}</textarea>
+              <label for="inputEdit">Content</label>
+          </div>
+          <button class="btn btn-outline" onclick="putRequest('/${route.split("/").pop()}')"> Update </button>
+      </div>
+    </div>
+  </div>
+    `);
+}
+
+async function putRequest(route, data) {
+  const [author, title, introduction, content] = $("div.form-update .form-control").map(function() {
+    return $(this).val();
+  });
+  
+  const result = await axios.put(route, {
+    data: {
+      author: author,
+      title: title,
+      introduction: introduction,
+      content: content
+    }
+  });
+  window.location.replace("/admin");
 }
